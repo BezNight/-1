e645716ccc516f89a27f4176522c33138b201f12
BezNight <Leonidd2006@gmail.com>
2025-09-17 13:23:39 +0500

    Третий и четвёртые принты

---
+++ Абрамов Леонид.py
@@ -0,0 +1,7 @@
+def main():
+    print('My first git program')
+    print('Abramov Leonid') #Первое изменение для коммита
+    print('Текущая дата: 17.09.2025')
+    print('If I had given up, today would not have arrived')
+if __name__ == '__main__': #Защита кода от выполнения при импорте модуля.
+    main()
