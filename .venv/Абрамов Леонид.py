6d5702b8bc8471939f3d4e57aec18d048dd91cf9
BezNight <Leonidd2006@gmail.com>
2025-09-17 12:38:41 +0500

    Второй принт

--- .venv/Абрамов Леонид.py
+++ .venv/Абрамов Леонид.py
@@ -1,5 +1,6 @@
 def main():
     print('My first git program')
     print('Abramov Leonid') #Первое изменение для коммита
+    print('ИВТ-ИВС 202Б Первая подгруппа')
 if __name__ == '__main__': #Защита кода от выполнения при импорте модуля.
     main()
