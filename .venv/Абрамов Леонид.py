a44ad3cd2bebc61eade27dd85b5fe0686647d097
BezNight <Leonidd2006@gmail.com>
2025-09-17 12:30:40 +0500

    Один из принтов для требований

---
+++ .venv/Абрамов Леонид.py
@@ -0,0 +1,5 @@
+def main():
+    print('My first git program')
+    print('Abramov Leonid') #Первое изменение для коммита
+if __name__ == '__main__': #Защита кода от выполнения при импорте модуля.
+    main()
