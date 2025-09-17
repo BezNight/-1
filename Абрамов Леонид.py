ca076225830ac9e18e77310495d3f24ca3b4db6b
BezNight <73055917+BezNight@users.noreply.github.com>
2025-09-17 13:30:55 +0500

    Update Абрамов Леонид.py
    Я тут песню разделил

--- Абрамов Леонид.py
+++ Абрамов Леонид.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('Abramov Leonid') #Первое изменение для коммита
     print('Текущая дата: 17.09.2025')
-    print('If I had given up, today would not have arrived')
+    print('If I had given up')
+    print('today would not have arrived')
 if __name__ == '__main__': #Защита кода от выполнения при импорте модуля.
     main()
